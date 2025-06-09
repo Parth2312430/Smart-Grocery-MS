7937a871ad268bb955d8b56a8e1dc90c1ecfb8bd
Parth2312430 <bscs2312430@szabist.pk>
2025-06-09 11:59:32 +0000

    Create Script and View

--- SGAPI/index.js
+++ SGAPI/index.js
@@ -1,1 +1,123 @@
- 
+ const express = require('express');
+const cors = require ('cors');
+const pool = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/',async(req,res)=>{
+    try{
+        res.json('WELCOME TO SG API');
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+    
+});
+
+app.get('/Customers',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from customers');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/categories',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from categories');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/discounts',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from discounts');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/employees',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from employees');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/inventory',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from inventory');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/orderdetails',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from orderdetails');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/orders',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from orders');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/payments',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from payments');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/products',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from products');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/suppliers',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from suppliers');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Connected Succefully....on PORT ${PORT}`);
+});
+
